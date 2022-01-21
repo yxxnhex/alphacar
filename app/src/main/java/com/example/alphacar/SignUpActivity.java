ea3bccbe849dbfd67be10f24fe90e69ad27d2929
smhrd <rhskgus048@naver.com>
2022-01-21 16:14:49 +0900

    singUp.xml 수정

--- app/src/main/java/com/example/alphacar/SignUpActivity.java
+++ app/src/main/java/com/example/alphacar/SignUpActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SingUpActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.singup);
+        setContentView(R.layout.signup);
     }
 }
